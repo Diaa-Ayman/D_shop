7f21b71c3d499e2421ab95a90b258bd21abdf726
Diaa-Ayman <abugamel53@gmail.com>
2022-03-23 11:00:46 +0200

    MY D_SHOP

---
+++ src/components/ui/Rating.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import StarIcon from './Staricon';
+
+const Rating = () => {
+  return (
+    <Fragment>
+      <StarIcon />
+      <StarIcon />
+      <StarIcon />
+      <StarIcon />
+      <StarIcon />
+    </Fragment>
+  );
+};
+
+export default Rating;
